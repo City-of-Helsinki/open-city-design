56fcaaca0782e9ac3a4337630e05001fbd7b0e2f
Tomi Sjöblom <tomi.sjoblom@siili.com>
2017-12-14 14:25:17 +0200

    Merge preparations

--- src/components/Sidebar/development/SidebarPagetemplates.js
+++ src/components/Sidebar/development/SidebarPagetemplates.js
@@ -31,109 +31,13 @@ class SidebarPagetemplates extends Component {
       'components': [
    
         {
-          link: 'Alerts',
-          URL: '/components/alerts/'
+          link: 'Todo Page',
+          URL: 'development/components/alerts'
         },
         {
-          link: 'Badge',
-          URL: '/components/badge/'
-        },
-        {
-          link: 'Breadcrumbs',
-          URL: '/components/breadcrumbs/'
-        },
-        {
-          link: 'Buttons',
-          URL: '/components/buttons/'
-        },
-        {
-          link: 'Button Dropdown',
-          URL: '/components/button-dropdown/'
-        },
-        {
-          link: 'Button Group',
-          URL: '/components/button-group/'
-        },
-        {
-          link: 'Card',
-          URL: '/components/card/'
-        },
-        {
-          link: 'Collapse',
-          URL: '/components/collapse/',
-        },
-        {
-          link: 'Carousel',
-          URL: '/components/carousel/'
-        },
-        {
-          link: 'Dropdowns',
-          URL: '/components/dropdowns/'
-        },
-        {
-          link: 'Fade',
-          URL: '/components/fade/'
-        },
-        {
-          link: 'Form',
-          URL: '/components/form/'
-        },
-        {
-          link: 'Input Group',
-          URL: '/components/input-group/'
-        },
-        {
-          link: 'Jumbotron',
-          URL: '/components/jumbotron/'
-        },
-        {
-          link: 'Layout',
-          URL: '/components/layout/'
-        },
-        {
-          link: 'List Group',
-          URL: '/components/listgroup/'
-        },
-        {
-          link: 'Media',
-          URL: '/components/media/'
-        },
-        {
-          link: 'Modals',
-          URL: '/components/modals/'
-        },
-        {
-          link: 'Navbar',
-          URL: '/components/navbar/'
-        },
-        {
-          link: 'Navs',
-          URL: '/components/navs/'
-        },
-        {
-          link: 'Pagination',
-          URL: '/components/pagination/'
-        },
-        {
-          link: 'Popovers',
-          URL: '/components/popovers/'
-        },
-        {
-          link: 'Progress',
-          URL: '/components/progress/'
-        },
-        {
-          link: 'Tables',
-          URL: '/components/tables/'
-        },
-        {
-          link: 'Tabs',
-          URL: '/components/tabs/'
-        },
-        {
-          link: 'Tooltips',
-          URL: '/components/tooltips/'
-        },
+          link: 'Card View page',
+          URL: 'development/components/alerts'
+        }
       ],
     
     };
--- src/components/Sidebar/development/SidebarPatterns.js
+++ src/components/Sidebar/development/SidebarPatterns.js
@@ -30,110 +30,17 @@ class SidebarPatterns extends Component {
     }
     const sidebarLinks = {
       'components': [
-   
         {
-          link: 'Alerts',
-          URL: '/components/alerts/'
+          link: 'Task List',
+          URL: 'development/components/tooltips/'
         },
         {
-          link: 'Badge',
-          URL: '/components/badge/'
-        },
+          link: 'Card Group Slider',
+          URL: 'development/components/tooltips/'
+        },    
         {
-          link: 'Breadcrumbs',
-          URL: '/components/breadcrumbs/'
-        },
-        {
-          link: 'Buttons',
-          URL: '/components/buttons/'
-        },
-        {
-          link: 'Button Dropdown',
-          URL: '/components/button-dropdown/'
-        },
-        {
-          link: 'Button Group',
-          URL: '/components/button-group/'
-        },
-        {
-          link: 'Card',
-          URL: '/components/card/'
-        },
-        {
-          link: 'Collapse',
-          URL: '/components/collapse/',
-        },
-        {
-          link: 'Carousel',
-          URL: '/components/carousel/'
-        },
-        {
-          link: 'Dropdowns',
-          URL: '/components/dropdowns/'
-        },
-        {
-          link: 'Fade',
-          URL: '/components/fade/'
-        },
-        {
-          link: 'Form',
-          URL: '/components/form/'
-        },
-        {
-          link: 'Input Group',
-          URL: '/components/input-group/'
-        },
-        {
-          link: 'Jumbotron',
-          URL: '/components/jumbotron/'
-        },
-        {
-          link: 'Layout',
-          URL: '/components/layout/'
-        },
-        {
-          link: 'List Group',
-          URL: '/components/listgroup/'
-        },
-        {
-          link: 'Media',
-          URL: '/components/media/'
-        },
-        {
-          link: 'Modals',
-          URL: '/components/modals/'
-        },
-        {
-          link: 'Navbar',
-          URL: '/components/navbar/'
-        },
-        {
-          link: 'Navs',
-          URL: '/components/navs/'
-        },
-        {
-          link: 'Pagination',
-          URL: '/components/pagination/'
-        },
-        {
-          link: 'Popovers',
-          URL: '/components/popovers/'
-        },
-        {
-          link: 'Progress',
-          URL: '/components/progress/'
-        },
-        {
-          link: 'Tables',
-          URL: '/components/tables/'
-        },
-        {
-          link: 'Tabs',
-          URL: '/components/tabs/'
-        },
-        {
-          link: 'Tooltips',
-          URL: '/components/tooltips/'
+          link: 'Card Group',
+          URL: 'development/components/tooltips/'
         },
       ],
     
