c9cc293f2b9bba2963f6fbe9828eaaf3eed09692
Tero Tikkanen <tero.tikkanen@gmail.com>
2018-06-28 17:45:29 +0300

    Fix checkbox ids in forms docs

--- src/components/docs/components/Form/FormValidation.js
+++ src/components/docs/components/Form/FormValidation.js
@@ -6,42 +6,42 @@ export default class Example extends React.Component {
     return (
       <Form>
        <FormGroup>
-          <Label for="exampleEmail">Input without validation</Label>
+          <Label for="exampleEmailVal">Input without validation</Label>
           <Input />
           <FormFeedback>You will not be able to see this</FormFeedback>
           <FormText>Example help text that remains unchanged.</FormText>
         </FormGroup>
         <FormGroup>
-          <Label for="exampleEmail">Valid input</Label>
+          <Label for="exampleEmailVal">Valid input</Label>
           <Input valid />
           <FormFeedback valid>Sweet! that name is available</FormFeedback>
           <FormText>Example help text that remains unchanged.</FormText>
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">Invalid input</Label>
+          <Label for="examplePasswordVal">Invalid input</Label>
           <Input invalid />
           <FormFeedback>Oh noes! that name is already taken</FormFeedback>
           <FormText>Example help text that remains unchanged.</FormText>
         </FormGroup>
         <FormGroup>
-          <Label for="exampleCheckbox">Checkboxes</Label>
+          <Label for="exampleCheckboxVal">Checkboxes</Label>
           <div>
-            <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" invalid/>
-            <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Or this one" invalid/>
+            <CustomInput type="checkbox" id="exampleCustomCheckboxVal" label="Check this custom checkbox" invalid/>
+            <CustomInput type="checkbox" id="exampleCustomCheckboxVal2" label="Or this one" invalid/>
           </div>
           <FormFeedback>Please make selection.</FormFeedback>
         </FormGroup>
         <FormGroup>
-          <Label for="exampleCheckbox">Radios</Label>
+          <Label for="exampleRadioVal">Radios</Label>
           <div>
-            <CustomInput type="radio" id="exampleCustomRadio" name="customRadio" label="Select this custom radio" invalid/>
-            <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="Or this one" invalid/>
+            <CustomInput type="radio" id="exampleCustomRadioVal" name="customRadioVal" label="Select this custom radio" invalid/>
+            <CustomInput type="radio" id="exampleCustomRadioVal2" name="customRadioVal" label="Or this one" invalid/>
           </div>
           <FormFeedback>Please make selection.</FormFeedback>
         </FormGroup>
         <FormGroup>
-          <Label for="exampleCustomSelect">Custom Select</Label>
-          <CustomInput type="select" id="exampleCustomSelect" name="customSelect" invalid>
+          <Label for="exampleCustomSelectVal">Custom Select</Label>
+          <CustomInput type="select" id="exampleCustomSelectVal" name="customSelectVal" invalid>
             <option value="">Select</option>
             <option>Value 1</option>
             <option>Value 2</option>
